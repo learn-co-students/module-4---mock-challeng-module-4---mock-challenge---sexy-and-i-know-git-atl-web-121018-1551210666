6ea864b92fa9b94dc52be13a52c8ef84e94f11bb
wgeorge1990 <williameverettgeorge@icloud.com>
2019-02-26 15:59:35 -0500

    solution

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 
+
+
 class PokemonCard extends React.Component {
+  state = {
+    toggle: true
+  }
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
   render() {
+    const { name, sprites, stats } = this.props.poke
+
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img
+              src={this.state.toggle ? sprites.front : sprites.back}
+              alt="alt"
+              onClick={this.toggle} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {stats[5].value}
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,9 +4,11 @@ import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
   render() {
+    console.log(this.props.pokemons)
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {this.props.pokemons.map(poke => <PokemonCard poke={poke} key={poke.id} />)}
       </Card.Group>
     )
   }
