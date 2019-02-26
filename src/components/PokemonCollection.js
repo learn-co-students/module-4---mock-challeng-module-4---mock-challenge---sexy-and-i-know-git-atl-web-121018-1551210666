e2b9dfa219a633b72903b8bc24aa95a20a5fef6d
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-26 15:45:43 -0500

    done without bonus

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,14 +2,22 @@ import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
-class PokemonCollection extends React.Component {
-  render() {
+const PokemonCollection = (props) => {
+  const pokeList = props.pokemon.map(poke =>
+    <PokemonCard
+      key={poke.id}
+      name={poke.name}
+      sprites={poke.sprites}
+      hp = {poke.stats[poke.stats.length - 1]} />)
+
     return (
-      <Card.Group itemsPerRow={6}>
+      <div>
         <h1>Hello From Pokemon Collection</h1>
-      </Card.Group>
+          <Card.Group itemsPerRow={6}>
+            {pokeList}
+          </Card.Group>
+      </div>
     )
-  }
 }
 
 export default PokemonCollection
