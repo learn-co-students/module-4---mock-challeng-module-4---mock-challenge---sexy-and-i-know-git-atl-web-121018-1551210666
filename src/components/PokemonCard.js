a89e08e56ffd64ed776d63a7208d7ff297066273
Arthur Torres <arthurtorres002@gmail.com>
2019-02-26 16:09:14 -0500

    cards show and search partially working

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,46 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.front = props.poke.sprites.front
+    this.back = props.poke.sprites.back
+    this.hp = props.poke.stats.find(stat => stat.name === "hp").value
+
+    this.state = {
+      image: this.front
+    }
+  }
+
+  flip = () => {
+    let side
+
+    if (this.state.image === this.front) {
+      side = this.back
+    } else {
+      side = this.front
+    }
+
+    this.setState({
+      image: side
+    })
+  }
+
   render() {
     return (
-      <Card>
+      <Card onClick={this.flip}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.image} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.poke.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.hp}
             </span>
           </div>
         </div>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,14 +5,42 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: []
+  }
+
+  // URL = 'http://localhost:3000/pokemon'
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon').then(res => res.json()).then(pokemon => {
+      this.setState({
+        pokemon: pokemon
+      })
+    })
+  }
+
+  //filter is one way, can fix it by fetching again
+
+  search = (event) => {
+    const filterText = event.target.value
+
+    const newPokemon = this.state.pokemon.filter(poke => poke.name.includes(filterText))
+
+    this.setState({
+      pokemon: newPokemon
+    })
+  }
+
+
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.search} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon}/>
         <br />
         <PokemonForm />
       </div>
